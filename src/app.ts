b051110d5182378e512a40714a4900f062cd9a28
alaa <alaa.mady@coformatique.com>
2023-05-26 02:27:04 +0300

    fix: enable cors

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(
   express.urlencoded({ limit: "10mb", extended: true, parameterLimit: 50000 })
 );
-app.use(cors({ origin: corsUrl, optionsSuccessStatus: 200 }));
+app.use(cors());
 
 // Routes
 app.use("/", routes);
